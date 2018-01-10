fbc70943743187a015514ecf658c5e2f05cd5494
mardanga <gaitan@CESVIARG>
2018-01-10 15:06:06 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  lat = -34.4075956;
+  lng = -58.977779;
+  zoom: 16;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDG2-0crsUU1Q4s8OxykarpSU-fT-6yPuE'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
